12b562a04f66de57b560f71ad49122feb7b66429
Andrea Broccoletti <andrea.broccoletti@brockdev.it>
2022-08-01 21:17:56 +0200

    Added openAPI doc standards

--- iliadapi.py
+++ iliadapi.py
@@ -2,19 +2,41 @@ import uvicorn
 from fastapi import FastAPI
 from backend.routes import router
 from fastapi.responses import JSONResponse
+from fastapi.openapi.utils import get_openapi
 from fastapi.exceptions import RequestValidationError
+
 
 
 app = FastAPI()
 origins = ["http://localhost:8000"]
 
-
 app.include_router(router)
+
 
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request, exc):
     return JSONResponse(status_code=422, content={'error': "Unprocessable Entity, check your request"})
+
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Iliad UnAPI",
+        version="1.0.0",
+        description="Unofficial Iliad API. Iliad S.P.A. is not responsible in any way.",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
+
 
 
 if __name__ == '__main__':
