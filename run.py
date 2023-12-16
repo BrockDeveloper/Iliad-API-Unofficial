acd641fcf1e6cf7201fc11c650f8f08f7b7637e6
Andrea Broccoletti <andrea.broccoletti@brockdev.it>
2023-12-16 22:25:00 +0100

    Fix: run file useless

--- run.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-from params import SERVER_HOST, SERVER_PORT
-
-
-
-if __name__ == '__main__':
-
-    # Run the API with uvicorn server
-    uvicorn.run("main:app", host=SERVER_HOST, port=SERVER_PORT, reload=False)
