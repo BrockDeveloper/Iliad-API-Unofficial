c5445adeb0d20096800cd6d85e24220eb27575f9
Andrea Broccoletti <andrea.broccoletti@brockdev.it>
2023-12-16 22:20:11 +0100

    Feat: cors middleware

--- main.py
+++ main.py
@@ -21,6 +21,22 @@ async def validation_exception_handler(request, exc):
 
 
 
+@app.middleware("http")
+async def add_cors_header(request, call_next):
+
+    '''
+    Consente di aggiungere l'header "Access-Control-Allow-Origin" alla risposta
+    di ogni richiesta HTTP, ovvero consente di effettuare richieste da un
+    dominio diverso da quello del server.
+    '''
+
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+
+    return response
+
+
+
 # custom openapi specification
 def custom_openapi():
     if app.openapi_schema:
