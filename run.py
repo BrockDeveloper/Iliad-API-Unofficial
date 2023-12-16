614a9887df169368c177eca6954f07f08067d2b9
Andrea Broccoletti <andrea.broccoletti@brockdev.it>
2023-12-16 22:19:29 +0100

    Fix: uvicorn reload mode

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ from params import SERVER_HOST, SERVER_PORT
 if __name__ == '__main__':
 
     # Run the API with uvicorn server
-    uvicorn.run("main:app", host=SERVER_HOST, port=SERVER_PORT, log_level="info", reload=True)
+    uvicorn.run("main:app", host=SERVER_HOST, port=SERVER_PORT, reload=False)
